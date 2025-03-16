40a85e57c50f69fa358b417a11b531388176f583
HieuNguyen <hieu1282003@gmail.com>
2025-03-16 21:08:58 +0700

    Reset SDL2

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,40 @@
 #include <SDL.h>
-#include <iostream>
+#include <SDL_image.h>
 
-const int WIDTH = 25;
-const int HEIGHT = 15;
+int main(int argc, char* args []) {
+    SDL_Init(SDL_INIT_VIDEO);
 
-int main(int argc, char *argv[]) {
-    SDL_Init(SDL_INIT_EVERYTHING);
-    SDL_Window *window = SDL_CreateWindow("Hello World!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    SDL_SetRenderDrawColor(renderer, 223, 0, 0, 255);
-    SDL_RenderClear(renderer);
-    SDL_RenderPresent(renderer);
-    SDL_Delay(5000);
+    SDL_Window* window = SDL_CreateWindow("Blank Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
+    if (window == nullptr) {
+        SDL_Log("Failed to create window: %s", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (renderer == nullptr) {
+        SDL_Log("Failed to create renderer: %s", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    bool running = true;
+    SDL_Event event;
+
+    while (running) {
+        // Close window with any input
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT || event.type == SDL_KEYDOWN || event.type == SDL_MOUSEBUTTONDOWN) {
+                running = false;
+                break;
+            }
+        }
+
+        SDL_SetRenderDrawColor(renderer, 100, 100, 180, 255); // Set the background color to purple
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
+    }
+
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
